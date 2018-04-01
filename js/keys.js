1b9e8d90336e177ee1a9532cac279a7f2ba9731b
Anna Burley <anna.burley3@gmail.com>
2018-04-01 11:46:22 +0100

    Returned html file, added normalise and cleaned key.js file

--- js/keys.js
+++ js/keys.js
@@ -8,7 +8,6 @@ document.addEventListener("keydown", lightenUpImage);
 function lightenUpCircle(e) {
     event.preventDefault();
     var element = document.getElementById('circle-large');
-    //e = event.which || event.keyCode;
     if(e.keyCode == 77 || e.keyCode == 109)
         element.classList.toggle('transition');
 }
@@ -17,24 +16,22 @@ function lightenUpCircle(e) {
 function lightenUpMovies(e) {
     event.preventDefault();
     var element = document.getElementById('movies-block');
-    //e = event.which || event.keyCode;
     if(e.keyCode == 70 || e.keyCode == 102)
-
     element.classList.toggle('movies-lightened');
 }
 
+//Pressing D,E,V characters simultaneously will change all three images
 var images = {68: false, 69: false, 86: false};
 function lightenUpImage (e) {
-        if (e.keyCode in images) {
-            images[e.keyCode] = true;
-            if (images[68] && images[69] && images[86]) {
-                var element1 = document.getElementById('image-1');
-                var element2 = document.getElementById('image-2');
-                var element3 = document.getElementById('image-3');
-                element1.classList.toggle('image-lightened');
-                element2.classList.toggle('image-lightened');
-                element3.classList.toggle('image-lightened');
-            }
+    if (e.keyCode in images) {
+        images[e.keyCode] = true;
+        if (images[68] && images[69] && images[86]) {
+            var element1 = document.getElementById('image-1');
+            var element2 = document.getElementById('image-2');
+            var element3 = document.getElementById('image-3');
+            element1.classList.toggle('image-lightened');
+            element2.classList.toggle('image-lightened');
+            element3.classList.toggle('image-lightened');
         }
     }
-
+}
